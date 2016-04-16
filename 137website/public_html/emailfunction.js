002dfad4a7832a0200b7a2b7cd38838a3f8b856f
LitlJess <jparhusi@uci.edu>
2016-04-15 21:20:01 -0700

    Changed the image on homepage and changed the title of website
    Also changed how the links are displayed

--- 137website/public_html/emailfunction.js
+++ 137website/public_html/emailfunction.js
@@ -12,8 +12,15 @@ function verifyinput()
 function sendemail()
 {
     var email = document.getElementById('email');
-    var yourMessage = "";
-    var subject = "Order Confirmation";
+    /*var product = "insert product name here";
+    var name = document.getElementById('name');
+    var creditcardnum = document.getElementById('creditcardnum').substring(12);*/
+    var yourMessage = "Order"/*"Order for "+product
+                        +"\n Name: "+name
+                        +"\n Credit Card Number: xxxxxx"+creditcardnum
+                        +"\n"+"\n"
+                        +"Thank you for shopping at 33 Bookstore!"*/;
+    var subject = "Order Confirmation for "/*+product*/;
     document.location.href = "mailto:"+email+"?subject="
         + encodeURIComponent(subject)
         + "&body=" + encodeURIComponent(yourMessage);
